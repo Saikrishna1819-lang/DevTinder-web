1bf4b270aafade5d67b811ad81f8d265ab169ce7
Saikrishna1819-lang <krishnasai69219@gmail.com>
2025-06-01 20:48:47 +0530

    all added

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -12,8 +12,7 @@ const Body = () => {
   const dispatch=useDispatch();
   const navigate=useNavigate();
   const fetchUser=async()=>{
-    if(userData){
-      return 
+    if (userData && Object.keys(userData).length > 0) return;
     }
     try{
 
