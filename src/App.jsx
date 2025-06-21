45aec244fcc93fd6e630a6676095d2211c63a710
Saikrishna1819-lang <krishnasai69219@gmail.com>
2025-06-21 19:29:04 +0530

    Fix routing for Vite on Render with static.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,46 @@
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import Body from "./components/Body";
+// import Login from "./components/Login";
+// import Profile from "./components/Profile";
+// import { Provider } from "react-redux";
+// import appStore from "./utils/appStore";
+// import Feed from "./components/Feed";
+// import Connections from "./components/Connections";
+// import Requests from "./components/Requests";
+// import Chat from "./components/Chat";
+// import LandingPage from "./components/LandingPage";
+
+// function App() {
+//   return (
+//     <div className="w-full min-h-screen font-sans bg-[#232429]">
+//       <Provider store={appStore}>
+//         <BrowserRouter>
+//           <Routes>
+//             {/* Public Routes */}
+//             <Route path="/" element={<LandingPage />} />
+           
+
+            
+//             <Route path="/" element={<Body />}>
+//              <Route path="/login" element={<Login />} />
+//               <Route path="feed" element={<Feed />} />
+//               <Route path="connections" element={<Connections />} />
+//               <Route path="requests" element={<Requests />} />
+//               <Route path="chat/:targetUserId" element={<Chat />} />
+//               <Route path="profile" element={<Profile />} />
+//             </Route>
+//           </Routes>
+//         </BrowserRouter>
+//       </Provider>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Body from "./components/Body";
 import Login from "./components/Login";
@@ -18,11 +61,10 @@ function App() {
           <Routes>
             {/* Public Routes */}
             <Route path="/" element={<LandingPage />} />
-            {/* <Route path="/login" element={<Login />} /> */}
+            <Route path="/login" element={<Login />} />
 
-            {/* Protected / Authenticated Routes inside Body Layout */}
+            {/* Private routes inside Body layout */}
             <Route path="/" element={<Body />}>
-             <Route path="/login" element={<Login />} />
               <Route path="feed" element={<Feed />} />
               <Route path="connections" element={<Connections />} />
               <Route path="requests" element={<Requests />} />
